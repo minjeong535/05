8ab07fd3ff65922e7939f832ae9e937d64136f18
minjeong535 <55127421+minjeong535@users.noreply.github.com>
2019-10-10 15:04:23 +0900

    1

--- main.c
+++ main.c
@@ -4,26 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int answer=59;
-	int input;
-	int cnt=0;
-	
-	do
-	{
-	
-	   printf("input a number :");
-	   scanf("%d", & input);
-	   if(input< answer)
-	   {
-	   	 printf("low!\n");
-	   }
-	   else if (input> answer)
-	   {
-	   	 printf("high!\n");
-	   }
-	     cnt++;
-    }
-    while( input != answer );
-    printf("Congratulation.\n");
+
+    int input;
+    
+    printf("input is a number :");
+    scanf("%d", &input);
+    
+    if(input>0)
+        printf("positive\n");
+    else
+        printf("non-positive\n");
+     
 	return 0;
 }
